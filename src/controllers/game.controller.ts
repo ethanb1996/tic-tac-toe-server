ce9f759fddf09fc516959bdce03dfd34e29f7a73
ethanb1996 <bonanethan@gmail.com>
2024-07-12 13:26:58 +0300

    add logical for makeMove

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -10,13 +10,21 @@ let gameState: GameState = [
   ['', '', '']
 ];
 
-
+const checkWinCondition = (gameState:any) => {}
 const initGame = (req: Request, res: Response): void => {
   res.json(gameState);
 };
 
 const makeMove = (req: Request, res: Response): void => {
-  
+  const { row, col, player } = req.body;
+  if (gameState[row][col] === '') {
+    gameState[row][col] = player;
+    const winner = checkWinCondition(gameState);
+    io.emit('moveMade', { gameState, winner });
+    res.json({ gameState, winner });
+  } else {
+    res.status(400).send('Invalid move');
+  }
 };
 
 const resetGame = (req: Request, res: Response): void => {
