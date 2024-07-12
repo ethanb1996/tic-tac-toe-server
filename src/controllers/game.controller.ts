7dd14c82868eda2fef9d93e06f907d955df54da7
ethanb1996 <bonanethan@gmail.com>
2024-07-12 15:34:02 +0300

    check win condition

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -10,16 +10,50 @@ let gameState: GameState = [
   ['', '', '']
 ];
 
-const checkWinCondition = (gameState:any) => {}
+const checkWinCondition = (row:number,col: number, player:string) => {
+  
+  const checkRow = () =>{
+    for (let i = 0; i < gameState[0].length; i++) {
+      console.log(row,i, gameState[row][i])
+      if (gameState[row][i] != player)
+        return null
+    }
+    return player
+  }
+  const checkCol = () =>{
+    for (let i = 0; i < gameState[0].length; i++) {
+      if (gameState[i][col] != player)
+        return null
+    }
+    return player;
+  }
+  const checkDiag = () =>{
+    for (let i = 0; i < gameState[0].length; i++) {
+      if (gameState[i][i] != player)
+        return null
+    }
+    return player;
+  }
+  const checkAntiDiag = () =>{
+    for (let i = 0; i < gameState[0].length; i++) {
+      if (gameState[i][gameState[0].length - 1 - i] != player)
+      return null
+    }
+    return player;
+  }
+
+  return [checkRow(),checkCol(),checkDiag(),checkAntiDiag()].some(val => val === player)? player: null
+}
 const initGame = (req: Request, res: Response): void => {
   res.json(gameState);
 };
 
 const makeMove = (req: Request, res: Response): void => {
-  const { row, col, player } = req.body;
+  const { row,col, player } = req.body;
   if (gameState[row][col] === '') {
     gameState[row][col] = player;
-    const winner = checkWinCondition(gameState);
+    console.log(row, col, player)
+    const winner = checkWinCondition(row,col,player);
     io.emit('moveMade', { gameState, winner });
     res.json({ gameState, winner });
   } else {
