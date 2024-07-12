c5be6000ea7696be9f182510262cb9eaa7ec2726
ethanb1996 <bonanethan@gmail.com>
2024-07-12 13:00:42 +0300

    add routes

--- src/routes/game.routes.ts
+++ src/routes/game.routes.ts
@@ -4,5 +4,7 @@ import { initGame, makeMove, resetGame } from '../controllers/game.controller';
 const gameRoutes = Router();
 
 gameRoutes.get('/init', initGame);
+gameRoutes.post('/move', makeMove);
+gameRoutes.post('/reset', resetGame);
 
 export { gameRoutes };
